ea9c9208c37379b9ff2eb4045a43c759d0c0d239
iuri <bentoiuri@gmail.com>
2024-07-24 15:02:50 -0300

    add BarberController

---
+++ src/main/java/com/iuri/barbershop/controller/BarberController.java
@@ -0,0 +1,40 @@
+package com.iuri.barbershop.controller;
+
+import com.iuri.barbershop.dto.barber.BarberRequest;
+import com.iuri.barbershop.dto.barber.BarberResponse;
+import com.iuri.barbershop.service.BarberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+public class BarberController {
+
+    @Autowired
+    private BarberService barberService;
+
+    @PostMapping
+    public BarberResponse save(@RequestBody BarberRequest barberRequest){
+        return barberService.save(barberRequest);
+    }
+
+    @GetMapping
+    public List<BarberResponse> findAll(){
+        return barberService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public BarberResponse findById(@PathVariable Integer id){
+        return BarberResponse.convert(barberService.findById(id));
+    }
+
+    @PutMapping("/{id}")
+    public BarberResponse update(@RequestBody BarberRequest barberRequest, @PathVariable Integer id){
+        return BarberResponse.convert(barberService.update(barberRequest, id));
+    }
+
+    @DeleteMapping
+    public void delete(@PathVariable Integer id){
+        barberService.delete(id);
+    }
+}
